48fada45e04e133d93a0198441da43bfc4cf1fed
fk2019 <fk2019@gmail.com>
2023-09-10 20:13:50 +0300

    Add translations

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -30,7 +30,7 @@ def get_locale() -> str:
 @app.route('/', strict_slashes=False)
 def holberton() -> str:
     """Print welcome to holberton"""
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
