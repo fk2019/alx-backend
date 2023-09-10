c0fd295168fdd33192c3b8e703223b53b3a66775
fk2019 <fk2019@gmail.com>
2023-09-10 13:25:57 +0300

    Add 2-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,13 +2,25 @@
 """Start a flask app
 """
 from flask import Flask, render_template
+from flask_babel import Babel
 app = Flask(__name__)
+
+
+class Config():
+    """Config class"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route('/', strict_slashes=False)
 def holberton() -> str:
     """Print welcome to holberton"""
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == "__main__":
